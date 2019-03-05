640506ab32f01ec4b75bed4d6004428e1db30189
klkirstyleeskl <k.lees@btinternet.com>
2019-03-05 15:35:35 +0000

    adds wand length

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -6,8 +6,12 @@ const CharacterDetail = ({character}) => {
     if (!character) return null;
     return(
         <dl>
-            <dt>Name</dt>
+            <dt>Name:</dt>
             <dd>{character.name}</dd>
+            <dt>Eye Colour:</dt>
+            <dd>{character.eyeColour}</dd>
+            <dt>Wand Length:</dt>
+            <dd>{character.wand.length}</dd>
 
 
 
