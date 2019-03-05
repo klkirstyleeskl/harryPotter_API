5d15c6530a321ee1825caa2fb9202a076ccad1de
klkirstyleeskl <k.lees@btinternet.com>
2019-03-05 15:28:21 +0000

    populates select

--- src/components/CharacterSelect.js
+++ src/components/CharacterSelect.js
@@ -2,5 +2,21 @@ import React from "react";
 
 
 const CharacterSelect = (props) => {
-    const options = props.
+    const options = props.characters.map((character, index) =>{
+        return <option value={index} key={index}>{character.name}</option>
+    })
+
+    function handleChange(event){
+        props.onCharacterSelected(event.target.value);
+    }
+
+    return (
+        <select id="character-selected" onChange={handleChange} defaultValue="default">
+        <option disabled value="default">Choose a babe...</option>
+            {options}
+        </select>
+    )
 }
+
+
+export default CharacterSelect
